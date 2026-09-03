63d8142deaeb03ad93d88b60ac37b32a711cbbd3
qium9495-creator <qium9495@gmail.com>
2026-09-03 16:38:03 +0800

    Update PRODUCT room taxonomy

--- translations.js
+++ translations.js
@@ -270,3 +270,21 @@ const DETAIL_PAGINATION_COPY={
  zh:{'detail.variant.sizes':'不同尺寸','detail.variant.colors':'不同颜色','detail.variant.materials':'不同材质','catalog.pagePrev':'上一页','catalog.pageNext':'下一页','catalog.pageGo':'跳转到第 {page} 页'}
 };
 Object.keys(I18N).forEach(lang=>Object.assign(I18N[lang],DETAIL_PAGINATION_COPY.en,DETAIL_PAGINATION_COPY[lang]||{}));
+
+const PRODUCT_TAXONOMY_COPY={
+ en:{
+  'type.sofas':'Sofas','type.armchairs':'Armchairs','type.coffeeTables':'Coffee Tables','type.tvStands':'TV Stands','type.sideTables':'Side Tables','type.bookcases':'Bookcases','type.ottomans':'Ottomans','type.consoleTables':'Console Tables',
+  'type.beds':'Beds','type.nightstands':'Nightstands','type.dressers':'Dressers','type.wardrobes':'Wardrobes','type.dressingTables':'Dressing Tables','type.benches':'Benches','type.mirrors':'Mirrors',
+  'type.diningTables':'Dining Tables','type.diningChairs':'Dining Chairs','type.sideboards':'Sideboards','type.barCarts':'Bar Carts','type.barStools':'Bar Stools','type.displayCabinets':'Display Cabinets','type.diningBenches':'Benches',
+  'type.officeDesks':'Office Desks','type.officeChairs':'Office Chairs','type.officeBookcases':'Bookcases','type.filingCabinets':'Filing Cabinets','type.credenzas':'Credenzas','type.deskAccessories':'Desk Accessories',
+  'type.entryConsoleTables':'Console Tables','type.shoeCabinets':'Shoe Cabinets','type.coatRacks':'Coat Racks','type.entryBenches':'Benches','type.entryMirrors':'Mirrors','type.umbrellaStands':'Umbrella Stands'
+ },
+ zh:{
+  'type.sofas':'沙发','type.armchairs':'扶手椅 / 休闲椅','type.coffeeTables':'茶几','type.tvStands':'电视柜','type.sideTables':'边几 / 角几','type.bookcases':'书柜 / 置物架','type.ottomans':'脚凳 / 坐墩','type.consoleTables':'条案 / 玄关桌',
+  'type.beds':'床','type.nightstands':'床头柜','type.dressers':'斗柜 / 抽屉柜','type.wardrobes':'衣柜','type.dressingTables':'梳妆台','type.benches':'床尾凳','type.mirrors':'镜子',
+  'type.diningTables':'餐桌','type.diningChairs':'餐椅','type.sideboards':'餐边柜','type.barCarts':'酒水车','type.barStools':'吧台椅','type.displayCabinets':'展示柜 / 瓷器柜','type.diningBenches':'餐厅长凳',
+  'type.officeDesks':'办公桌 / 写字台','type.officeChairs':'办公椅 / 书桌椅','type.officeBookcases':'书柜 / 书架','type.filingCabinets':'文件柜','type.credenzas':'矮柜 / 储物柜','type.deskAccessories':'桌面收纳 / 配件',
+  'type.entryConsoleTables':'玄关桌 / 条案','type.shoeCabinets':'鞋柜','type.coatRacks':'衣帽架','type.entryBenches':'换鞋凳','type.entryMirrors':'墙面镜','type.umbrellaStands':'雨伞架'
+ }
+};
+Object.keys(I18N).forEach(lang=>Object.assign(I18N[lang],PRODUCT_TAXONOMY_COPY.en,PRODUCT_TAXONOMY_COPY[lang]||{}));
